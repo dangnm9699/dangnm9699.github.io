dec15e741391fc2298f947b8fc5c6e804fbdcb1f
Dang Minh Nguyen <dangnm@bsscommerce.com>
2022-12-06 21:40:24 +0700

    reset project

--- context/title.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-const TitleContext = createContext();
-
-const TitleContextProvider = TitleContext.Provider;
-
-export { TitleContext, TitleContextProvider };
